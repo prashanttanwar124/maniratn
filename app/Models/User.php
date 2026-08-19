bebc4c234e8fbcf732019aa4910d291cf29e721a
prashanttanwar124 <prashanttanwar124@gmail.com>
2026-08-19 20:43:15 +0530

    Add TwoFactorAuthenticatable trait to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,14 +8,12 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-
-    use HasRoles;
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles, TwoFactorAuthenticatable;
 
     /**
      * The attributes that are mass assignable.
