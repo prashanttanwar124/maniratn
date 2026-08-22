051439b8d7ed81f850d9abd00605292ea16f793a
prashanttanwar124 <prashanttanwar124@gmail.com>
2026-08-22 09:16:58 +0530

    Update KaratSetu ERP favicons and logo mark to gold-bordered squircle background design

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,10 +14,11 @@
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=3">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=3">
-    <link rel="icon" href="/favicon.ico?v=3" sizes="any">
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png?v=3">
+    <link rel="icon" type="image/png" sizes="512x512" href="/favicon-512.png?v=4">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=4">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=4">
+    <link rel="icon" href="/favicon.ico?v=4" sizes="any">
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png?v=4">
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
